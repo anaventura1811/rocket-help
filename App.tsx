59258c7cb14451e14347d1d06377bcf9085e53ee
Ana Ventura <anaventura1811@gmail.com>
2022-07-23 19:31:29 -0300

    inicia estilização da tela home

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { NativeBaseProvider, StatusBar } from 'native-base';
 import { THEME } from './src/styles/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular});
@@ -15,7 +16,7 @@ export default function App() {
         backgroundColor={"transparent"}
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Home /> : <Loading />}
     </NativeBaseProvider>
   
   );
---
+++ src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import { VStack } from "native-base";
+
+export function Home() {
+
+  return (
+    <VStack flex={1} pb={6} bg={"gray.700"}>
+
+    </VStack>
+  )
+}
