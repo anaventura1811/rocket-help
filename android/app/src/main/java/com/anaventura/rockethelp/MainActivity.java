46714a283954f2400fd4d89d63ee008f8df2dc3a
Ana Ventura <anaventura1811@gmail.com>
2022-07-26 15:00:22 -0300

    novo build do app

--- android/app/src/main/java/com/anaventura/rockethelp/MainActivity.java
+++ android/app/src/main/java/com/anaventura/rockethelp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rockethelp;
+package com.anaventura.rockethelp;
 
 import android.os.Build;
 import android.os.Bundle;
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -4,15 +4,18 @@ import { Input } from '../components/Input';
 import { Envelope, Key } from 'phosphor-react-native';
 import { Button } from '../components/Button';
 import { useState } from 'react';
+import auth from '@react-native-firebase/auth';
+import { Alert } from 'react-native';
 
 export function SignIn() {
-  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { colors } = useTheme();
 
   function handleSignIn() {
-    console.log("email: ", name);
-    console.log("senha: ", password);
+    if (!email || !password) {
+      return Alert.alert('Login', 'Informe email e senha');
+    }
   }
 
   return (
@@ -23,7 +26,7 @@ export function SignIn() {
       </Heading>
       <Input
         placeholder="Email"
-        onChangeText={(email) => setName(email)}
+        onChangeText={(userEmail) => setEmail(userEmail)}
         mb={4}
         InputLeftElement={<Icon ml={4} as={<Envelope color={colors.gray[300]} />} />}
       />
